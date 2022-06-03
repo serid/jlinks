f3cc37bfadefd90b044ebc6e4c1cc23a7d418d12
Sergey <serridd@gmail.com>
2022-06-03 16:03:15 +0300

    Gc stats.

--- src/main/kotlin/jitrs/util/Util.kt
+++ src/main/kotlin/jitrs/util/Util.kt
@@ -1,5 +1,7 @@
 package jitrs.util
 
+import java.lang.management.ManagementFactory
+
 fun matchPrefix(string: String, from: Int, prefix: String): Boolean {
     if (string.length - from < prefix.length) return false
     for (i in prefix.indices) {
@@ -11,4 +13,15 @@ fun matchPrefix(string: String, from: Int, prefix: String): Boolean {
 fun myAssert(condition: Boolean) {
     if (!condition)
         throw AssertionError("Assertion failed")
+}
+
+fun printGCStats() {
+    var totalGarbageCollections: Long = 0
+    var garbageCollectionTime: Long = 0
+    for (gc in ManagementFactory.getGarbageCollectorMXBeans()) {
+        totalGarbageCollections += gc.collectionCount
+        garbageCollectionTime += gc.collectionTime
+    }
+    println("Total Garbage Collections: $totalGarbageCollections")
+    println("Total Garbage Collection Time (ms): $garbageCollectionTime")
 }
